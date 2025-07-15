6ea970d6b6f7113ee652fb204e080e2713f031e7
antoniofelices <antonio@studiomoare.com>
2025-07-15 12:57:52 +0200

    Remove the middle Axios functions and join them into services

--- src/services/moviesService.ts
+++ src/services/moviesService.ts
@@ -6,53 +6,31 @@ import type {
 
 import { apiMoviesData } from '@/config/moviesAxiosConfig'
 
-const fetchDiscoverMovies = (page: number = 1) =>
-    apiMoviesData.get(
+const getMoviesByDiscover = async (page: number = 1) => {
+    const response = await apiMoviesData.get(
         `/discover/movie?include_adult=false&language=en-US&page=${page}&sort_by=popularity.desc`
     )
-
-const fetchMovieById = (id: number): Promise<MovieProps> =>
-    apiMoviesData.get(`/movie/${id}`)
-
-const fetchCastByMovie = (id: number): Promise<CreditsMovieProps> =>
-    apiMoviesData.get(`/movie/${id}/credits`)
-
-const fetchPersonById = (id: number): Promise<PersonProps> =>
-    apiMoviesData.get(`/person/${id}`)
-
-const fetcMoviesByPerson = (id: number): Promise<PersonProps> =>
-    apiMoviesData.get(`/person/${id}/movie_credits`)
-
-const getMoviesByDiscover = async (page: number = 1) => {
-    const response = await fetchDiscoverMovies(page)
-    // const response = await apiMoviesData.get(
-    //     `/discover/movie?include_adult=false&language=en-US&page=${page}&sort_by=popularity.desc`
-    // )
-    return response
+    return response.data
 }
 
-const getSingleMovie = async (id: number) => {
-    const response = await fetchMovieById(id)
-    // const response = await apiMoviesData.get(`/movie/${id}`)
-    return response
+const getSingleMovie = async (id: number): Promise<MovieProps> => {
+    const response = await apiMoviesData.get(`/movie/${id}`)
+    return response.data
 }
 
-const getCastMovie = async (id: number) => {
-    const response = await fetchCastByMovie(id)
-    // const response = await apiMoviesData.get(`/movie/${id}/credits`)
-    return response
+const getCastMovie = async (id: number): Promise<CreditsMovieProps> => {
+    const response = await apiMoviesData.get(`/movie/${id}/credits`)
+    return response.data
 }
 
-const getPerson = async (id: number) => {
-    const response = await fetchPersonById(id)
-    // const response = await apiMoviesData.get(`/person/${id}`)
-    return response
+const getPerson = async (id: number): Promise<PersonProps> => {
+    const response = await apiMoviesData.get(`/person/${id}`)
+    return response.data
 }
 
-const getMoviesByPerson = async (id: number) => {
-    const response = await fetcMoviesByPerson(id)
-    // const response = await apiMoviesData.get(`/person/${id}/movie_credits`)
-    return response
+const getMoviesByPerson = async (id: number): Promise<PersonProps> => {
+    const response = await apiMoviesData.get(`/person/${id}/movie_credits`)
+    return response.data
 }
 
 export {
